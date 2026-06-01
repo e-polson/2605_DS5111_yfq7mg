e2bcc71fd314a77191c3645ef25f98a9db69f0b9
e-polson <emmacpolson@gmail.com>
2026-06-01 01:18:51 +0000

    Adding my script which checks inputted IDs to ensure they are valid YouTube IDs. If they are valid, they are printed. Invalid IDs are stored in a log file called 'pipeline_autid.log'. Note this file is overwritten every time the command runs on a new input file. I am also uploading 'test_ids' and 'youtube_ids' which are two files I used to check my code.

---
+++ clean_ids.py
@@ -0,0 +1,30 @@
+import re
+import sys
+import logging
+
+log_file = logging.getLogger(__name__)
+logging.basicConfig(filename = 'pipeline_autid.log', encoding = 'utf-8', filemode = 'w', level = logging.INFO, format = '%(message)s')
+
+
+def check_id(id):
+	if re.match(r"[A-Za-z0-9_0]{11}$", id):
+		print(id, end="")
+	else:
+		log_file.info(id)
+
+if sys.stdin.isatty():
+	while True:
+		try:
+			current_id = input()
+			check_id(current_id)
+			print()
+		except KeyboardInterrupt:
+			sys.exit(0)
+
+else:
+	try:
+		for current_id in sys.stdin:
+			check_id(current_id)
+
+	except KeyboardInterrupt:
+		sys.exit(0)
