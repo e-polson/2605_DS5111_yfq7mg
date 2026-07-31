5245b1495cafc4a7ca4bf9d903635273e4e5db19
e-polson <emmacpolson@gmail.com>
2026-07-31 21:32:27 +0000

    Fixing file

--- models/fct_book_mentions.sql
+++ models/fct_book_mentions.sql
@@ -4,5 +4,6 @@ SELECT
     VIDEO_ID,
     f.value::STRING AS book_title,
     INSERTED_AT AS PROCESSED_AT
-FROM {{ ref('stg_youtube_transcripts') }}
+FROM {{ ref('stg_youtube_transcripts') }},
+LATERAL FLATTEN(input => BOOK_NAMES_ARRAY) f
 WHERE book_title IS NOT NULL
